34d6870f165e81cce466a7442c507bd2ae634c95
Gabrielle Silva <gabi.w.c.araujo@gmail.com>
2026-08-28 23:38:53 -0300

    Primeiro commit

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Importações do Bootstrap e Bootstrap Icons
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).mount('#app')
